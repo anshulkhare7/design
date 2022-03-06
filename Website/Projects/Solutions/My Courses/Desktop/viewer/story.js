a09f8faf67f4779757859bd30ce7c2415f5bcbfd
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-03-06 15:40:13 +0530

    refactoring

---
+++ Website/Projects/Solutions/My Courses/Desktop/viewer/story.js
@@ -0,0 +1,100 @@
+var story = {
+ "docName": "web-mycourses-desktop",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "Web_Mycourses_Desktop",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "63E8D83A-3EA9-4D0B-9894-59C0BA9F8091",
+   "groupID": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
+   "index": 0,
+   "image": "mycourses.png",
+   "image2x": "mycourses@2x.png",
+   "width": 1440,
+   "height": 1923,
+   "x": -1768,
+   "y": -550,
+   "title": "MyCourses",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "A803089D-649D-4FB0-95B2-A370419DD7AA",
+   "groupID": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
+   "index": 1,
+   "image": "mycoursevideo_chapters.png",
+   "image2x": "mycoursevideo_chapters@2x.png",
+   "width": 1440,
+   "height": 1479,
+   "x": -265,
+   "y": -550,
+   "title": "MyCourseVideo/chapters",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "B18AAECA-8D8B-4CE7-AEB4-728462E9CF7C",
+   "groupID": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
+   "index": 2,
+   "image": "mycoursevideo_chapters_topics.png",
+   "image2x": "mycoursevideo_chapters_topics@2x.png",
+   "width": 1440,
+   "height": 1479,
+   "x": 1296,
+   "y": -550,
+   "title": "MyCourseVideo/chapters/topics",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
+   "name": "Page 1"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 3
+}
