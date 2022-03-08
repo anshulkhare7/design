855482739b837c583f01e7c937e7c471f10e82f9
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-03-08 23:24:22 +0530

    Cart page added

---
+++ Android/Quotes/viewer/story.js
@@ -0,0 +1,53 @@
+var story = {
+ "docName": "ap-app-2",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "AP App 2",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "F6A47078-D739-49A2-B26F-473B43E5ACA9",
+   "groupID": "74E2AC9A-484E-4FE9-A541-81CA82F3AF8F",
+   "index": 0,
+   "image": "quotes.png",
+   "image2x": "quotes@2x.png",
+   "width": 412,
+   "height": 869,
+   "x": -1614,
+   "y": -640,
+   "title": "Quotes",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "74E2AC9A-484E-4FE9-A541-81CA82F3AF8F",
+   "name": "Exports"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 1
+}
---
+++ Android/solutions(player design)/viewer/story.js
@@ -0,0 +1,81 @@
+var story = {
+ "docName": "mycoursesplayer",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "MyCoursesPlayer",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "221FA5D7-ED08-42F6-A3D1-9EF501C05B49",
+   "groupID": "A3D5715F-DBEC-4A34-A303-172207D82200",
+   "index": 0,
+   "image": "courseplayer_enrichinglife.png",
+   "image2x": "courseplayer_enrichinglife@2x.png",
+   "width": 412,
+   "height": 640,
+   "x": 59,
+   "y": -11,
+   "title": "CoursePlayer_EnrichingLife",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "07E9E601-1D9E-466F-98EE-EB5B18564175",
+   "groupID": "A3D5715F-DBEC-4A34-A303-172207D82200",
+   "index": 1,
+   "image": "courseplayer_1.2parentalloveonpausemode.png",
+   "image2x": "courseplayer_1.2parentalloveonpausemode@2x.png",
+   "width": 412,
+   "height": 845,
+   "x": 498,
+   "y": -11,
+   "title": "CoursePlayer_1.2ParentalLoveOnPauseMode",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "A3D5715F-DBEC-4A34-A303-172207D82200",
+   "name": "Page 1"
+  },
+  {
+   "id": "3E05552A-C96A-4CAA-B4AA-53D5CDEEFEC1",
+   "name": "Symbols"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 2
+}
