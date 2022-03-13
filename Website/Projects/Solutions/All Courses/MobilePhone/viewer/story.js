da7c42d00e2a61024db4afd28d4e10899856f338
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-03-13 10:59:08 +0530

    Added empty cart for website mobile

--- Website/Projects/Solutions/All Courses/MobilePhone/viewer/story.js
+++ Website/Projects/Solutions/All Courses/MobilePhone/viewer/story.js
@@ -19,9 +19,32 @@ var story = {
  "highlightLinks": false,
  "pages": [
   {
-   "id": "FD870334-4B76-46CF-BD78-4F0BB9F3AE9A",
+   "id": "5C84B9F5-5991-44AE-B2FF-7248599DD23A",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
    "index": 0,
+   "image": "empty_cart.png",
+   "image2x": "empty_cart@2x.png",
+   "width": 412,
+   "height": 803,
+   "x": -2640,
+   "y": -987,
+   "title": "Empty cart",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "FD870334-4B76-46CF-BD78-4F0BB9F3AE9A",
+   "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
+   "index": 1,
    "image": "footer.png",
    "image2x": "footer@2x.png",
    "width": 412,
@@ -44,7 +67,7 @@ var story = {
   {
    "id": "C7EE2AE2-8F7D-4788-8EEF-AA702EC0520F",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 1,
+   "index": 2,
    "image": "search_disable.png",
    "image2x": "search_disable@2x.png",
    "width": 412,
@@ -67,7 +90,7 @@ var story = {
   {
    "id": "EF7B6BB4-625F-4501-8274-7C1A4103E943",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 2,
+   "index": 3,
    "image": "search_enable.png",
    "image2x": "search_enable@2x.png",
    "width": 412,
@@ -90,7 +113,7 @@ var story = {
   {
    "id": "1A194068-6561-417F-834F-AD009FF0250C",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 3,
+   "index": 4,
    "image": "search_categoryselected.png",
    "image2x": "search_categoryselected@2x.png",
    "width": 412,
@@ -113,7 +136,7 @@ var story = {
   {
    "id": "F387A6C2-7691-4AFE-BD01-841FC0C50544",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 4,
+   "index": 5,
    "image": "home.png",
    "image2x": "home@2x.png",
    "width": 412,
@@ -136,7 +159,7 @@ var story = {
   {
    "id": "6540697E-4983-4991-AEFB-169668C0E154",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 5,
+   "index": 6,
    "image": "course.png",
    "image2x": "course@2x.png",
    "width": 412,
@@ -159,7 +182,7 @@ var story = {
   {
    "id": "E6697111-9C1B-472E-B389-4B7F5C6797CC",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 6,
+   "index": 7,
    "image": "topic.png",
    "image2x": "topic@2x.png",
    "width": 412,
@@ -182,7 +205,7 @@ var story = {
   {
    "id": "8134E1DE-8572-4CB4-8C10-DA365CFA6752",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 7,
+   "index": 8,
    "image": "series.png",
    "image2x": "series@2x.png",
    "width": 412,
@@ -205,7 +228,7 @@ var story = {
   {
    "id": "EF6A70FD-F780-4D82-AE87-3D06F0E17F17",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 8,
+   "index": 9,
    "image": "sidemenu2.png",
    "image2x": "sidemenu2@2x.png",
    "width": 412,
@@ -228,7 +251,7 @@ var story = {
   {
    "id": "A88D1040-9FE6-4C2A-98A7-4B41FB7C7A59",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 9,
+   "index": 10,
    "image": "sidemenu1.png",
    "image2x": "sidemenu1@2x.png",
    "width": 412,
@@ -251,7 +274,7 @@ var story = {
   {
    "id": "F6245FF0-EE6B-411A-A44D-1A34D82B1F44",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 10,
+   "index": 11,
    "image": "topic_copy.png",
    "image2x": "topic_copy@2x.png",
    "width": 412,
@@ -274,13 +297,13 @@ var story = {
   {
    "id": "1146DA2E-2917-4F16-AD1A-B7705949E29F",
    "groupID": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
-   "index": 11,
+   "index": 12,
    "image": "cart.png",
    "image2x": "cart@2x.png",
    "width": 412,
    "height": 1319,
-   "x": 2838,
-   "y": -954,
+   "x": 2836,
+   "y": -992,
    "title": "Cart",
    "transAnimType": 0,
    "layout": {
@@ -299,9 +322,13 @@ var story = {
   {
    "id": "C1EAA091-DFA3-414B-B1AD-88FD644C1B50",
    "name": "Page 1"
+  },
+  {
+   "id": "515888D0-8409-44D2-BBAA-EADBB9D20DF2",
+   "name": "Symbols"
   }
  ],
  "experimentalExisting": false,
  "startPageIndex": 0,
- "totalImages": 12
+ "totalImages": 13
 }
