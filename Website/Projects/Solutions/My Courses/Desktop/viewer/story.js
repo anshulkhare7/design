c07d8d200b7dab26c8088caf2102d36cb5e9ae9c
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-03-24 21:03:30 +0530

    Updated my courses design for web laptop

--- Website/Projects/Solutions/My Courses/Desktop/viewer/story.js
+++ Website/Projects/Solutions/My Courses/Desktop/viewer/story.js
@@ -1,5 +1,5 @@
 var story = {
- "docName": "web-mycourses-desktop",
+ "docName": "solutions-web",
  "docPath": "P_P_P",
  "docVersion": "V_V_V",
  "ownerName": "",
@@ -13,68 +13,22 @@ var story = {
  "fileType": "png",
  "disableHotspots": false,
  "zoomEnabled": true,
- "title": "Web_Mycourses_Desktop",
+ "title": "Solutions web",
  "layersExist": true,
  "centerContent": false,
  "highlightLinks": false,
  "pages": [
   {
-   "id": "C12168B3-84EA-4605-A0C5-9A95A0F8DFA9",
-   "groupID": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
+   "id": "D443BB58-DCE9-4F62-8532-85FBA7313012",
+   "groupID": "1C9065A5-D53C-4A58-9F08-AAFA3663E18A",
    "index": 0,
    "image": "mycourses.png",
    "image2x": "mycourses@2x.png",
    "width": 1440,
-   "height": 1712,
-   "x": -1768,
-   "y": -550,
+   "height": 2219,
+   "x": 10854,
+   "y": -2654,
    "title": "MyCourses",
-   "transAnimType": 0,
-   "layout": {
-    "offset": 0,
-    "totalWidth": 960,
-    "numberOfColumns": 12,
-    "columnWidth": 60,
-    "gutterWidth": 20
-   },
-   "type": "regular",
-   "fixedPanels": [],
-   "links": []
-  },
-  {
-   "id": "A803089D-649D-4FB0-95B2-A370419DD7AA",
-   "groupID": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
-   "index": 1,
-   "image": "mycoursevideo_chapters.png",
-   "image2x": "mycoursevideo_chapters@2x.png",
-   "width": 1440,
-   "height": 1479,
-   "x": -237,
-   "y": -550,
-   "title": "MyCourseVideo/chapters",
-   "transAnimType": 0,
-   "layout": {
-    "offset": 0,
-    "totalWidth": 960,
-    "numberOfColumns": 12,
-    "columnWidth": 60,
-    "gutterWidth": 20
-   },
-   "type": "regular",
-   "fixedPanels": [],
-   "links": []
-  },
-  {
-   "id": "B18AAECA-8D8B-4CE7-AEB4-728462E9CF7C",
-   "groupID": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
-   "index": 2,
-   "image": "mycoursevideo_chapters_topics.png",
-   "image2x": "mycoursevideo_chapters_topics@2x.png",
-   "width": 1440,
-   "height": 1479,
-   "x": 1296,
-   "y": -550,
-   "title": "MyCourseVideo/chapters/topics",
    "transAnimType": 0,
    "layout": {
     "offset": 0,
@@ -90,11 +44,11 @@ var story = {
  ],
  "groups": [
   {
-   "id": "435021E5-6D30-4E1C-BF56-43A50C4FC659",
-   "name": "Page 1"
+   "id": "1C9065A5-D53C-4A58-9F08-AAFA3663E18A",
+   "name": "ExportDesktop"
   }
  ],
  "experimentalExisting": false,
  "startPageIndex": 0,
- "totalImages": 3
+ "totalImages": 1
 }
