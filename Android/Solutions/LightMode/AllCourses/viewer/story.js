796c3fb2ce55c11c641f5ef5f4d797acf7525d17
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-04-09 13:48:03 +0530

    All courses home for app added

---
+++ Android/Solutions/LightMode/AllCourses/viewer/story.js
@@ -0,0 +1,77 @@
+var story = {
+ "docName": "app-1.1",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "App 1.1",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "61276159-C6B4-4E60-A83E-D3E3C8DC1C98",
+   "groupID": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "index": 0,
+   "image": "allcourseshome_when1collection.png",
+   "image2x": "allcourseshome_when1collection@2x.png",
+   "width": 412,
+   "height": 2177,
+   "x": -236,
+   "y": -759,
+   "title": "AllCoursesHome_when1Collection",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "37594C49-DB25-4402-8BB7-39C209CA8195",
+   "groupID": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "index": 1,
+   "image": "allcourseshome_whenmorethan1collection.png",
+   "image2x": "allcourseshome_whenmorethan1collection@2x.png",
+   "width": 412,
+   "height": 2146,
+   "x": 267,
+   "y": -759,
+   "title": "AllCoursesHome_whenMoreThan1Collection",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "name": "AllCourses"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 2
+}
