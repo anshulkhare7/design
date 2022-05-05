1be85140910cea040e22ac9565c623591aee03a3
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-05-05 15:27:18 +0530

    Loader design added

---
+++ Website/Projects/Solutions/LodingIndicator/data/story.js
@@ -0,0 +1,77 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "9352ED33-E57B-403A-BA84-CF1668340840",
+   "groupID": "ADC3AC3A-4038-4D56-9369-9E04A55EE925",
+   "index": 0,
+   "image": "loaderdesign.png",
+   "image2x": "loaderdesign@2x.png",
+   "width": 285,
+   "height": 436,
+   "x": 2300,
+   "y": -531,
+   "title": "LoaderDesign",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "BC85A00D-26FD-4208-84E8-FBD51DF1FEFD",
+   "groupID": "ADC3AC3A-4038-4D56-9369-9E04A55EE925",
+   "index": 1,
+   "image": "loadersample.png",
+   "image2x": "loadersample@2x.png",
+   "width": 1440,
+   "height": 976,
+   "x": 2659,
+   "y": -532,
+   "title": "LoaderSample",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "ADC3AC3A-4038-4D56-9369-9E04A55EE925",
+   "name": "Page 2"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 2
+}
