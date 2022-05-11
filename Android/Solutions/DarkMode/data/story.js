c6ce622878c15ba535b92a272d11a5e202889867
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-05-11 14:30:02 +0530

    Added 3 screens for solutions dark theme

---
+++ Android/Solutions/DarkMode/data/story.js
@@ -0,0 +1,100 @@
+var story = {
+ "docName": "app-1.1",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "App 1.1",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "75748C4B-C388-493D-80F9-303DD84427C3",
+   "groupID": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "index": 0,
+   "image": "mycourses_darkmode.png",
+   "image2x": "mycourses_darkmode@2x.png",
+   "width": 412,
+   "height": 1378,
+   "x": -2513,
+   "y": 2114,
+   "title": "MyCourses_DarkMode",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "0A01A954-3D95-4AC4-8317-C3DB49B5B121",
+   "groupID": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "index": 1,
+   "image": "coursepage_comesfromseriescourse.png",
+   "image2x": "coursepage_comesfromseriescourse@2x.png",
+   "width": 412,
+   "height": 1187,
+   "x": -2070,
+   "y": 2109,
+   "title": "CoursePage_ComesFromSeriesCourse",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "B2A5F0CE-5D3D-4851-A25C-64EB2F206715",
+   "groupID": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "index": 2,
+   "image": "seriespage.png",
+   "image2x": "seriespage@2x.png",
+   "width": 412,
+   "height": 1625,
+   "x": -1627,
+   "y": 2108,
+   "title": "SeriesPage",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "name": "AllCourses"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 3
+}
