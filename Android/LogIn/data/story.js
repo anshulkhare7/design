905a2a24a40b5eefdb21041654e1cc5f59506b96
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-05-24 13:10:17 +0530

    Added login design for android app

---
+++ Android/LogIn/data/story.js
@@ -0,0 +1,169 @@
+var story = {
+ "docName": "app-1.1",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "App 1.1",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "A10482AC-D15A-41AD-8521-77EA44181EFA",
+   "groupID": "243CF60E-AE25-4ABA-9355-DB1FD17717A4",
+   "index": 0,
+   "image": "register_or_sign_in_with_country_code.png",
+   "image2x": "register_or_sign_in_with_country_code@2x.png",
+   "width": 412,
+   "height": 914,
+   "x": -207,
+   "y": -458,
+   "title": "Register or Sign In with country code",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "D56C643B-493F-428C-ABF2-5A2434194D0B",
+   "groupID": "243CF60E-AE25-4ABA-9355-DB1FD17717A4",
+   "index": 1,
+   "image": "register_or_sign_in_with_country_code(1).png",
+   "image2x": "register_or_sign_in_with_country_code(1)@2x.png",
+   "width": 412,
+   "height": 914,
+   "x": 221,
+   "y": -458,
+   "title": "Register or Sign In with country code(1)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "CCB3578B-3C1B-4DF3-ACDD-5B9D5F1EDC7A",
+   "groupID": "243CF60E-AE25-4ABA-9355-DB1FD17717A4",
+   "index": 2,
+   "image": "otp_varification.png",
+   "image2x": "otp_varification@2x.png",
+   "width": 412,
+   "height": 914,
+   "x": 649,
+   "y": -458,
+   "title": "OTP varification",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "D73AD14B-1CE3-4FC2-BB76-A7DAD8BEBFF0",
+   "groupID": "243CF60E-AE25-4ABA-9355-DB1FD17717A4",
+   "index": 3,
+   "image": "resend_otp.png",
+   "image2x": "resend_otp@2x.png",
+   "width": 412,
+   "height": 914,
+   "x": 1092,
+   "y": -458,
+   "title": "Resend otp",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "D823CE37-EA91-4B67-84FB-070937917B00",
+   "groupID": "243CF60E-AE25-4ABA-9355-DB1FD17717A4",
+   "index": 4,
+   "image": "otp_verification_and_registration.png",
+   "image2x": "otp_verification_and_registration@2x.png",
+   "width": 412,
+   "height": 914,
+   "x": 1535,
+   "y": -458,
+   "title": "OTP verification and registration",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "01801505-AFE8-4899-AF3F-A716ED14AA5B",
+   "groupID": "243CF60E-AE25-4ABA-9355-DB1FD17717A4",
+   "index": 5,
+   "image": "sign_in.png",
+   "image2x": "sign_in@2x.png",
+   "width": 412,
+   "height": 914,
+   "x": 1964,
+   "y": -458,
+   "title": "Sign in",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "243CF60E-AE25-4ABA-9355-DB1FD17717A4",
+   "name": "LogIn"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 6
+}
