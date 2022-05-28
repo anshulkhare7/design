7972566616f180076faa9bba2465c34ba3a36a84
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-05-29 00:20:14 +0530

    added googlr signin icon

---
+++ Android/Solutions/LightMode/ChangedDesign/data/story.js
@@ -0,0 +1,77 @@
+var story = {
+ "docName": "app-1.1",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "App 1.1",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "91FBE4E9-7D46-4129-91DA-7C81FE92C1B6",
+   "groupID": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "index": 0,
+   "image": "seriespagenew.png",
+   "image2x": "seriespagenew@2x.png",
+   "width": 412,
+   "height": 830,
+   "x": 1634,
+   "y": -759,
+   "title": "SeriesPageNew",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "9423D00F-2DB2-4F6F-A0A7-FD3B8F2B68EE",
+   "groupID": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "index": 1,
+   "image": "courseplayer.png",
+   "image2x": "courseplayer@2x.png",
+   "width": 412,
+   "height": 861,
+   "x": 5887,
+   "y": -760,
+   "title": "CoursePlayer",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "F2459D88-8FA0-4995-8D9A-9AAD75E3359A",
+   "name": "AllCourses"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 2
+}
