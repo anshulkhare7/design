b1b32db569b14f7e4ac0eb96821adecfe0fee0c6
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-05-29 16:41:07 +0530

    Added main website home page for desktop

---
+++ Website/Projects/Main Website/Desktop/Home/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "F4BFBA1B-535B-4183-B333-6EBDFACED446",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 0,
+   "image": "website_home.png",
+   "image2x": "website_home@2x.png",
+   "width": 1440,
+   "height": 8606,
+   "x": 228,
+   "y": 1051,
+   "title": "website_home",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "name": "Home"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
