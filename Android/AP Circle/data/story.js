b13e0c07f7206ff21c8388d3bfc6df6bf77f1664
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-05-31 15:50:08 +0530

    Added AP Circle design

---
+++ Android/AP Circle/data/story.js
@@ -0,0 +1,100 @@
+var story = {
+ "docName": "app-1.1",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "App 1.1",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "BF8D5F67-8DD3-43B6-ADCB-4AE59CA662DC",
+   "groupID": "10F750CA-090D-417D-8C4C-1FB3EF7F617D",
+   "index": 0,
+   "image": "apcirclehome.png",
+   "image2x": "apcirclehome@2x.png",
+   "width": 412,
+   "height": 944,
+   "x": 189,
+   "y": -435,
+   "title": "APCircleHome",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "75B340F8-193D-4437-8778-359A0480F3E1",
+   "groupID": "10F750CA-090D-417D-8C4C-1FB3EF7F617D",
+   "index": 1,
+   "image": "post_search_by_date.png",
+   "image2x": "post_search_by_date@2x.png",
+   "width": 412,
+   "height": 869,
+   "x": 640,
+   "y": -435,
+   "title": "Post Search by date",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "76600ACD-1958-4707-8C92-20F70DC059EE",
+   "groupID": "10F750CA-090D-417D-8C4C-1FB3EF7F617D",
+   "index": 2,
+   "image": "search_result.png",
+   "image2x": "search_result@2x.png",
+   "width": 412,
+   "height": 944,
+   "x": 1078,
+   "y": -435,
+   "title": "Search result",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "10F750CA-090D-417D-8C4C-1FB3EF7F617D",
+   "name": "AP circle"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 3
+}
