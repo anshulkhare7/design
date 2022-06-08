f937d41cb77ba8863ee125496ef64ec0d94925cc
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-06-08 22:56:13 +0530

    Website Donate page design added

---
+++ Website/Projects/Main Website/Mobile/Donate/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "0BFADEE1-46CD-4286-B5C1-E8E17211585F",
+   "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "index": 0,
+   "image": "donate_home.png",
+   "image2x": "donate_home@2x.png",
+   "width": 412,
+   "height": 5790,
+   "x": -1522,
+   "y": 14155,
+   "title": "Donate Home",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "name": "Donate"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
