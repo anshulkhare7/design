ca04f5df45fff0d68d6f12f38e2f0526e7f3db78
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-06-11 15:10:08 +0530

    Refactoring donate page

--- Website/Projects/Main Website/Mobile/Donate/data/story.js
+++ Website/Projects/Main Website/Mobile/Donate/data/story.js
@@ -19,14 +19,14 @@ var story = {
  "highlightLinks": false,
  "pages": [
   {
-   "id": "726529FF-8F03-4549-A309-AF5905039497",
+   "id": "0BFADEE1-46CD-4286-B5C1-E8E17211585F",
    "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
    "index": 0,
    "image": "donate_home.png",
    "image2x": "donate_home@2x.png",
-   "width": 1440,
-   "height": 3898,
-   "x": -927,
+   "width": 412,
+   "height": 5790,
+   "x": -1433,
    "y": 14155,
    "title": "Donate Home",
    "transAnimType": 0,
