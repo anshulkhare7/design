ef1193e84256b0c7c9f863b0355a719c10b7e016
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-06-15 16:44:29 +0530

    Refactoring

---
+++ Website/Projects/Main Website/Mobile/Contribute/data/story.js
@@ -0,0 +1,77 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "32A17A11-B8FC-4FB9-A32C-BDE6EAE54394",
+   "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "index": 0,
+   "image": "campaign_list.png",
+   "image2x": "campaign_list@2x.png",
+   "width": 412,
+   "height": 2946,
+   "x": 931,
+   "y": 14155,
+   "title": "Campaign List",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "ED94A9FC-5ECF-4BDE-8C37-3E4936EFB4B5",
+   "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "index": 1,
+   "image": "campaign_home.png",
+   "image2x": "campaign_home@2x.png",
+   "width": 412,
+   "height": 4390,
+   "x": 4574,
+   "y": 14155,
+   "title": "Campaign Home",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "name": "Donate"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 2
+}
