e81be0efbcb8b7080be5e9265adbc550d1b7c523
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-06-21 07:01:43 +0530

    Invite page added for desktop

---
+++ Website/Projects/Main Website/Desktop/Invite/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "B9F7D796-3C9F-425D-9D5C-3CEFD5B737C6",
+   "groupID": "B452DD37-C395-4045-B830-47B22CB6D011",
+   "index": 0,
+   "image": "invite.png",
+   "image2x": "invite@2x.png",
+   "width": 1440,
+   "height": 2331,
+   "x": -720,
+   "y": -448,
+   "title": "Invite",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "B452DD37-C395-4045-B830-47B22CB6D011",
+   "name": "Invite"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
