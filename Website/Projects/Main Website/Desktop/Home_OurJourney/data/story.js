5fdd1d43a40d5cba3eb7316a24677cf82fe82606
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-06-29 10:44:21 +0530

    Our Journey design added

---
+++ Website/Projects/Main Website/Desktop/Home_OurJourney/data/story.js
@@ -0,0 +1,100 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "218DA3B0-27CB-4B7E-9E51-2A4E14A7DD53",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 0,
+   "image": "our_journey.png",
+   "image2x": "our_journey@2x.png",
+   "width": 1440,
+   "height": 383,
+   "x": 1657,
+   "y": 6967,
+   "title": "Our Journey",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "300E0AB6-402D-4D59-A552-9E736C8F3BDD",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 1,
+   "image": "our_journey(1).png",
+   "image2x": "our_journey(1)@2x.png",
+   "width": 1440,
+   "height": 383,
+   "x": 3125,
+   "y": 6967,
+   "title": "Our Journey(1)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "C7AF0FEB-CC11-4972-B53F-B54594359CC5",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 2,
+   "image": "our_journey(2).png",
+   "image2x": "our_journey(2)@2x.png",
+   "width": 1440,
+   "height": 383,
+   "x": 4613,
+   "y": 6967,
+   "title": "Our Journey(2)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "name": "Home"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 3
+}
