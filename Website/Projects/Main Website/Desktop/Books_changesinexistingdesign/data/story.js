5e680104c7603ef397976957e2e1bd516a37702b
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-02 14:01:36 +0530

    Added books page design changes for existing website

---
+++ Website/Projects/Main Website/Desktop/Books_changesinexistingdesign/data/story.js
@@ -0,0 +1,77 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "69695958-92D0-44FF-9C92-812F17A6FF56",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 0,
+   "image": "books_mobile.png",
+   "image2x": "books_mobile@2x.png",
+   "width": 320,
+   "height": 721,
+   "x": 2870,
+   "y": 1051,
+   "title": "Books_mobile",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "C3422AE6-393A-46B8-A1D2-E1B464692244",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 1,
+   "image": "books_desktop.png",
+   "image2x": "books_desktop@2x.png",
+   "width": 1440,
+   "height": 896,
+   "x": 3228,
+   "y": 1051,
+   "title": "Books_desktop",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "name": "Home"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 2
+}
