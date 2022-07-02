fe558741beb5f7b01f301442bc50f52a183937dd
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-02 16:52:54 +0530

    Added enquiry confirmation page

---
+++ Website/Projects/Main Website/Desktop/EnquiryConfirmation/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "8B0083B5-8970-450C-AC10-8841213E6452",
+   "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "index": 0,
+   "image": "enquiryconfirmation.png",
+   "image2x": "enquiryconfirmation@2x.png",
+   "width": 1440,
+   "height": 1107,
+   "x": 6746,
+   "y": 14155,
+   "title": "EnquiryConfirmation",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "name": "Donate"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
