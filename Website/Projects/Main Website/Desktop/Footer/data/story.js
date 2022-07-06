2ac2b82ead1b2142e5e3edaa2176d350111ae33e
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-07 00:27:38 +0530

    Footer design added

---
+++ Website/Projects/Main Website/Desktop/Footer/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "9B734DCC-9EC8-4A65-948C-044B4BE3E9CE",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 0,
+   "image": "footer.png",
+   "image2x": "footer@2x.png",
+   "width": 1440,
+   "height": 353,
+   "x": -2443,
+   "y": 12801,
+   "title": "Footer",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "name": "Home"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
---
+++ Website/Projects/Main Website/Mobile/Footer/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "55E6CC22-D34B-4E09-89D7-C6DD7E67CCBE",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 0,
+   "image": "footer.png",
+   "image2x": "footer@2x.png",
+   "width": 412,
+   "height": 1058,
+   "x": -2443,
+   "y": 13211,
+   "title": "Footer",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "name": "Home"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
