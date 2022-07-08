fcb11902e8318a3c607277102803ce673ff09eb0
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-08 16:23:11 +0530

    Refactoring website home

---
+++ Website/Projects/Main Website/Mobile/Home/data/story.js
@@ -0,0 +1,100 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "7758DCEA-0EE7-4F4B-987D-DAD128DECD81",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 0,
+   "image": "mobile_home.png",
+   "image2x": "mobile_home@2x.png",
+   "width": 412,
+   "height": 13077,
+   "x": -886,
+   "y": 1070,
+   "title": "Mobile_Home",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "FD9D6DB1-C25E-479D-A342-6C382B8FF340",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 1,
+   "image": "vedanta_mahotsav_when_mutiple_mahotsav.png",
+   "image2x": "vedanta_mahotsav_when_mutiple_mahotsav@2x.png",
+   "width": 412,
+   "height": 1005,
+   "x": -387,
+   "y": 1089,
+   "title": "Vedanta Mahotsav_When mutiple mahotsav",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "9F0EDCC6-A124-4797-BC44-495078FA8984",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 2,
+   "image": "upanishads_course.png",
+   "image2x": "upanishads_course@2x.png",
+   "width": 412,
+   "height": 1136,
+   "x": 147,
+   "y": 1089,
+   "title": "Upanishads course",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "name": "Home"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 3
+}
