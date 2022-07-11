26bd56ebf0edf0841a5571a00bf60f37da1cb972
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-11 15:10:25 +0530

    Added campaign home page

---
+++ Website/Projects/Main Website/Desktop/Campaign Home(Ghar Ghar Upanishad Abhiyan)/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "4438304A-AFC3-43ED-86FD-7C9E88892924",
+   "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "index": 0,
+   "image": "campaign_home(ghar_ghar_upanishad_abhiyan).png",
+   "image2x": "campaign_home(ghar_ghar_upanishad_abhiyan)@2x.png",
+   "width": 1440,
+   "height": 2557,
+   "x": 5486,
+   "y": 14155,
+   "title": "Campaign Home(Ghar Ghar Upanishad Abhiyan)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "name": "Donate"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
---
+++ Website/Projects/Main Website/Mobile/Campaign Home(Ghar Ghar Upanishad Abhiyan)/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "ED94A9FC-5ECF-4BDE-8C37-3E4936EFB4B5",
+   "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "index": 0,
+   "image": "campaign_home(ghar_ghar_upanishad_abhiyan).png",
+   "image2x": "campaign_home(ghar_ghar_upanishad_abhiyan)@2x.png",
+   "width": 412,
+   "height": 4390,
+   "x": 7000,
+   "y": 14155,
+   "title": "Campaign Home(Ghar Ghar Upanishad Abhiyan)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
+   "name": "Donate"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
