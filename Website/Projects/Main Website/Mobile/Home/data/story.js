2800e6e9fd3455b8262da47ea4aaf1cd0d24439d
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-11 12:44:43 +0530

    Refacroting mobile web

--- Website/Projects/Main Website/Mobile/Home/data/story.js
+++ Website/Projects/Main Website/Mobile/Home/data/story.js
@@ -25,56 +25,10 @@ var story = {
    "image": "mobile_home.png",
    "image2x": "mobile_home@2x.png",
    "width": 412,
-   "height": 13077,
+   "height": 12861,
    "x": -886,
    "y": 1070,
    "title": "Mobile_Home",
-   "transAnimType": 0,
-   "layout": {
-    "offset": 0,
-    "totalWidth": 960,
-    "numberOfColumns": 12,
-    "columnWidth": 60,
-    "gutterWidth": 20
-   },
-   "type": "regular",
-   "fixedPanels": [],
-   "links": []
-  },
-  {
-   "id": "FD9D6DB1-C25E-479D-A342-6C382B8FF340",
-   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
-   "index": 1,
-   "image": "vedanta_mahotsav_when_mutiple_mahotsav.png",
-   "image2x": "vedanta_mahotsav_when_mutiple_mahotsav@2x.png",
-   "width": 412,
-   "height": 1005,
-   "x": -387,
-   "y": 1089,
-   "title": "Vedanta Mahotsav_When mutiple mahotsav",
-   "transAnimType": 0,
-   "layout": {
-    "offset": 0,
-    "totalWidth": 960,
-    "numberOfColumns": 12,
-    "columnWidth": 60,
-    "gutterWidth": 20
-   },
-   "type": "regular",
-   "fixedPanels": [],
-   "links": []
-  },
-  {
-   "id": "9F0EDCC6-A124-4797-BC44-495078FA8984",
-   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
-   "index": 2,
-   "image": "upanishads_course.png",
-   "image2x": "upanishads_course@2x.png",
-   "width": 412,
-   "height": 1136,
-   "x": 147,
-   "y": 1089,
-   "title": "Upanishads course",
    "transAnimType": 0,
    "layout": {
     "offset": 0,
@@ -96,5 +50,5 @@ var story = {
  ],
  "experimentalExisting": false,
  "startPageIndex": 0,
- "totalImages": 3
+ "totalImages": 1
 }
