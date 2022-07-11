95aa76c17b807ccbb1d4ba5d2cde80c90853beb5
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-11 12:10:31 +0530

    Refactoring

--- Website/Projects/Main Website/Mobile/Contribute/data/story.js
+++ Website/Projects/Main Website/Mobile/Contribute/data/story.js
@@ -22,36 +22,13 @@ var story = {
    "id": "32A17A11-B8FC-4FB9-A32C-BDE6EAE54394",
    "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
    "index": 0,
-   "image": "campaign_list.png",
-   "image2x": "campaign_list@2x.png",
+   "image": "donate_english.png",
+   "image2x": "donate_english@2x.png",
    "width": 412,
-   "height": 2946,
-   "x": 931,
+   "height": 6052,
+   "x": 7537,
    "y": 14155,
-   "title": "Campaign List",
-   "transAnimType": 0,
-   "layout": {
-    "offset": 0,
-    "totalWidth": 960,
-    "numberOfColumns": 12,
-    "columnWidth": 60,
-    "gutterWidth": 20
-   },
-   "type": "regular",
-   "fixedPanels": [],
-   "links": []
-  },
-  {
-   "id": "ED94A9FC-5ECF-4BDE-8C37-3E4936EFB4B5",
-   "groupID": "268C3F54-BE56-4B71-BC3C-3659FE310F10",
-   "index": 1,
-   "image": "campaign_home.png",
-   "image2x": "campaign_home@2x.png",
-   "width": 412,
-   "height": 4390,
-   "x": 4574,
-   "y": 14155,
-   "title": "Campaign Home",
+   "title": "Donate_English",
    "transAnimType": 0,
    "layout": {
     "offset": 0,
@@ -73,5 +50,5 @@ var story = {
  ],
  "experimentalExisting": false,
  "startPageIndex": 0,
- "totalImages": 2
+ "totalImages": 1
 }
