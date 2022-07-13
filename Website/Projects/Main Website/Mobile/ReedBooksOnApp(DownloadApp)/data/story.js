9281fc2b582df5e3dde5f7a7fce6ac8cb4f398c9
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-13 09:21:16 +0530

    Uploaded Read books on app design

---
+++ Website/Projects/Main Website/Mobile/ReedBooksOnApp(DownloadApp)/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteap",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteAP",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "ADD69F58-24F0-43FF-AF49-2E26EBC33189",
+   "groupID": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "index": 0,
+   "image": "reedbooksonapp(downloadapp).png",
+   "image2x": "reedbooksonapp(downloadapp)@2x.png",
+   "width": 412,
+   "height": 892,
+   "x": 1660,
+   "y": 1120,
+   "title": "ReedBooksOnApp(DownloadApp)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3ABBB45E-FEEF-4B30-AE11-4B4F4A6EA77F",
+   "name": "Home"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
