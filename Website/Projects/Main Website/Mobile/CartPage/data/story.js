69326b7297a3499091749d1bc814cecbfe951327
Dipti Vaghasiya <diptipkl8@gmail.com>
2022-07-19 16:11:58 +0530

    Cart Page design added

---
+++ Website/Projects/Main Website/Mobile/CartPage/data/story.js
@@ -0,0 +1,54 @@
+var story = {
+ "docName": "websiteversion1.0.1",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "WebsiteVersion1.0.1",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "7E89F722-66D4-4D97-B83A-7533ABBD16AC",
+   "groupID": "D015481C-6BAB-461E-B563-D64E4C8A43D5",
+   "index": 0,
+   "image": "cartpage.png",
+   "image2x": "cartpage@2x.png",
+   "width": 412,
+   "height": 2297,
+   "x": 3731,
+   "y": -1179,
+   "title": "CartPage",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "D015481C-6BAB-461E-B563-D64E4C8A43D5",
+   "name": "Books"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 1
+}
